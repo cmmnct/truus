838b80f83489b581a437f969db51b5ef34196b05
Mark Nijenhuis <mark@cmmnct.nl>
2025-03-28 09:53:19 +0100

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+type card = {
+card1:string;
+card2:string;
+turned: boolean;
+hidden: boolean;
+set? : string
+}
+
+const animals = ["hen", "dog", "chick", "kitten", "veal", "horse"];
+
+const cards = ref<card[]>([])
+
+animals.map(item => cards.value.push({
+  card1: item,
+  card2: item,
+  turned: false,
+  hidden: false
+}));
+
+</script>
+
+<template>
+  <main>
+<div v-for="card in cards" :key="card.card1">
+  <img :src="'/img/'+ card.card1 + '.webp'" :alt="card.card1">
+
+</div>
+  </main>
+</template>
